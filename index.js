d2916c0a2b6a0680fbdc29fd472c7a92c1cf4bb5
Aleksej Kamynin <zendzirou@gmail.com>
2016-04-17 22:02:25 +0300

    нужно больше репозиториев

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var check_dirty_git = require('./check_dirty_git');
 var fns = [
   commits_on_file_today('~/.db/wiki', 'waiting.md', 'waiting'),
   check_dirty_git({
+    '~/.bu.bin': 'bin',
     '~/.db/gnucash': '$$$',
     '~/.db/wiki': 'wiki',
     '~/.db/dotfiles': '.files',
