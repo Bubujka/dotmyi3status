d1b233992ca5d1059264772612969de9e47e1d29
Aleksej Kamynin <zendzirou@gmail.com>
2016-09-26 01:40:34 +0300

    Работа над плагином для таймера

--- timer.js
+++ timer.js
@@ -1,8 +1,11 @@
 var fs = require('fs');
 var helpers = require('./helpers.js');
 var pth = '/home/bubujka/.timer';
+var nn = require('node-notifier');
+
 var seconds = 0;
 var diff = 0;
+var prevdiff = 0;
 var hide_timer = true;
 
 function _update_timer(){
@@ -12,6 +15,7 @@ function _update_timer(){
       return;
     }
 
+    prevdiff = diff;
     diff = Math.floor((new Date() - stat.mtime) / 1000);
 
     fs.readFile(pth, 'utf8', function (err,data) {
@@ -26,10 +30,23 @@ _update_timer();
 setInterval(_update_timer, 1000);
 
 module.exports = function(data){
-  var s = seconds - diff;
+  var prev, s;
   var prefix = '';
   var color;
+  prev = seconds - prevdiff;
+  s = seconds - diff;
   if (s<0){
+    if(prev >= 0){
+      if(!hide_timer){
+        nn.notify({
+          title: "Timer",
+          message: 'Время у таймера вышло.',
+          urgency: 'critical',
+          wait: true,
+          time: 3600* 1000
+        });
+      }
+    }
     prefix = '-';
     color = '#FCFC00';
   }else{
